d225a177b74a2dc1e00da9a25bb30835f7dbd5ec
esyeon <esyeon@amoeba.co.kr>
2019-06-13 15:45:34 +0900

    upload

--- assets/lib/default.js
+++ assets/lib/default.js
@@ -1,8 +1,72 @@
 $(document).ready(function () {
+    var $windowScrollTop = $(window).scrollTop();
+    var $headerHeight = $('header').outerHeight();
 
-    //a 이벤트 방지
-    $('a').click(function (e) {
-        e.preventDefault()
+    $('.btn_top').on('click',function(){
+        $('html,body').stop().animate({
+            scrollTop : 0
+        },500)
     })
 
+    $('nav > ul > li > a').on('click',function(){
+        var $this = $(this);
+        var $li = $this.parent();
+        var $liIdx = $li.index();
+        var $sectionIdx = $('.scroll').eq($liIdx);
+        var $offsetTop = $sectionIdx.offset().top - $headerHeight;
+
+        $('html, body').stop().animate({
+            'scrollTop' : $offsetTop
+        },750)
+
+    })
+
+    $(window).scroll(function(){
+        var $li = $('nav li');
+        var $content = $('.scroll');
+
+
+        $content.each(function(idx){
+
+            var $target = $content.eq(idx);
+            var $calcHeight = $target.offset().top - $headerHeight;
+            var $sectionFirstTop = $('.portfolio_wrap').offset().top - $headerHeight;
+
+            if ($calcHeight <= $(window).scrollTop()) {
+                $li.eq(idx).addClass('active').siblings().removeClass('active');
+            }else if($sectionFirstTop >= $(window).scrollTop()){
+                $li.removeClass('active');
+            }
+        })
+
+        if ( $(this).scrollTop() > 200 ) {
+            $( '.btn_top' ).fadeIn();
+        } else {
+            $( '.btn_top' ).fadeOut();
+        }
+    })
+
+    $('.btn_mobile_menu').on('click',function(){
+        var $this = $(this);
+        var $header = $this.parent();
+
+        $header.toggleClass('mini')
+        return false;
+    })
+
+
+
+
 });
+
+function addElement(){
+    var newImg = document.createElement('img');
+    var srcNode = document.createAttribute('src');
+    srcNode.value = "../assets/image/lsy_logo.png";
+    var heightNode= document.createAttribute('height');
+    heightNode.value = '100';
+    newImg.setAttributeNode(srcNode);
+    newImg.setAttributeNode(heightNode);
+    document.getElementById('main').appendChild(newImg);
+}
+
---
+++ assets/lib/xmlload.js
@@ -0,0 +1,43 @@
+//xml load
+
+var APP = APP || {};
+var APP = {
+    //project data
+    pjData : function(){
+        $.get("../assets/lib/all.xml", function(data){
+            $(data).find('pjList').each(function(){
+                var $pjList = $(this);
+                var $siteUrl = $pjList.find('siteUrl').text();
+                var $pjthumb = $pjList.find('pjthumb').text();
+                var $pjName = $pjList.find('pjName').text();
+                var $pjDate = $pjList.find('pjDate').text();
+                var $pjWork = $pjList.find('pjWork').text();
+
+                var listObj = '<a href="' + $siteUrl + '" class="default">';
+                if($siteUrl == ""){
+                    listObj += '<p>내부 사이트이므로 링크없음</p>';
+                }else{
+                    listObj = '<a href="' + $siteUrl + '" class="thumb" target="_blank" title="새창 열림">';
+                }
+                listObj += '<img src="../assets/image/thumb/' + $pjthumb + '"/>';
+                if($pjthumb == ""){
+                    $('.default').find('img').remove();
+                }
+                listObj += '<div class="detail_wrap">';
+                listObj += '<div class="detail">';
+                listObj += '<P class="project_name">' + $pjName + '</P>';
+                listObj += '<P class="date"><span>참여기간 : </span>' + $pjDate + '</P>';
+                listObj += '<P class="work"><span>수행업무 : </span>' + $pjWork + '</P>';
+                listObj += '</div>';
+                listObj += '</div>';
+                listObj += '</a>';
+
+                $('.portfolio_list').append($(listObj));
+            })
+        })
+    }
+}
+
+$(function() {
+    APP.pjData();
+});
